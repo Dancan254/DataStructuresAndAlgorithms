62dcc46011537915d43dad82c045c5ec81ffa5ac
Dancan254 <dancanian25@gmail.com>
2024-11-06 17:46:15 +0300

    feat: Add main method to test SingleLinkedList class

---
+++ src/DSA/LinkedList/SinglyLinkedlist/Main.java
@@ -0,0 +1,10 @@
+package DSA.LinkedList.SinglyLinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+       SingleLinkedList list = new SingleLinkedList();
+       list.append(20);
+       list.append(30);
+    }
+}
